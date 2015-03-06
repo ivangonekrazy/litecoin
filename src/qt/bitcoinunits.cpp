f36008a8c1392002b4f648cd3378c13f3bd48ff7
Ivan Tam <ivan@loggly.com>
2015-03-06 13:42:37 -0800

    LTC --> LGC
    Rename the currency code

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("LGC");
+    case mBTC: return QString("mLGC");
+    case uBTC: return QString::fromUtf8("μLGC");
     default: return QString("???");
     }
 }
